e40a5b2f99ed1623034e2d65052701815244dd8c
Leela Dhoj Lama <ldlama@Leelas-MacBook-Pro.local>
2019-11-07 12:10:26 +0545

    navigation added and polls added to redux

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-const NavBar = () => <div>NavBar Works</div>;
+import {logout} from '../store/actions'
 
-export default NavBar;
+const NavBar = ({auth,logout}) => (
+    <div>
+        <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><button onClick={logout}>Logout</button></li>
+        </ul>
+        {auth.isAuthenticated && <p>Logged in as {auth.user.username}</p>}
+    </div>
+);
+
+export default connect(store=>({auth:store.auth}),{logout})(NavBar);
